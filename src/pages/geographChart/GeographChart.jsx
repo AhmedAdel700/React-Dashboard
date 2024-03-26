045ed4b63e2594ac0d209e204a5c188da1d8dbe5
Ahmed Adel <AhmedAdel.Engineer1@gmail.com>
2024-03-26 17:47:42 +0200

    Fixed The Title In The Geograph Page

--- src/pages/geographChart/GeographChart.jsx
+++ src/pages/geographChart/GeographChart.jsx
@@ -116,16 +116,7 @@ export default function GeographChart({ isDashboard = false }) {
     },
   };
   return (
-    <Stack
-      sx={{
-        height: isDashboard ? "450px" : "80vh",
-        borderTop: isDashboard
-          ? null
-          : `1px solid ${theme.palette.text.primary}`,
-        borderBottom: isDashboard
-          ? null
-          : `1px solid ${theme.palette.text.primary}`,
-      }}
+    <Box
       style={{
         width: isDashboard ? "100%" : "calc(100% - 64x)",
         marginLeft: isDashboard ? "0" : "64px",
@@ -139,107 +130,119 @@ export default function GeographChart({ isDashboard = false }) {
           />
         </Box>
       )}
-      <ResponsiveChoropleth
-        theme={barTheme}
-        data={data}
-        features={geoData.features}
-        projectionScale={isDashboard ? 100 : 150}
-        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        colors="nivo"
-        domain={[0, 1000000]}
-        unknownColor="#666666"
-        label="properties.name"
-        valueFormat=".2s"
-        projectionTranslation={[0.5, 0.5]}
-        projectionRotation={[0, 0, 0]}
-        enableGraticule={false}
-        graticuleLineColor="#dddddd"
-        borderWidth={0.5}
-        borderColor="#152538"
-        defs={[
-          {
-            id: "dots",
-            type: "patternDots",
-            background: "inherit",
-            color: "#38bcb2",
-            size: 4,
-            padding: 1,
-            stagger: true,
-          },
-          {
-            id: "lines",
-            type: "patternLines",
-            background: "inherit",
-            color: "#eed312",
-            rotation: -45,
-            lineWidth: 6,
-            spacing: 10,
-          },
-          {
-            id: "gradient",
-            type: "linearGradient",
-            colors: [
-              {
-                offset: 0,
-                color: "#000",
-              },
-              {
-                offset: 100,
-                color: "inherit",
-              },
-            ],
-          },
-        ]}
-        fill={[
-          {
-            match: {
-              id: "CAN",
+      <Stack
+        sx={{
+          height: isDashboard ? "450px" : "80vh",
+          borderTop: isDashboard
+            ? null
+            : `1px solid ${theme.palette.text.primary}`,
+          borderBottom: isDashboard
+            ? null
+            : `1px solid ${theme.palette.text.primary}`,
+        }}
+      >
+        <ResponsiveChoropleth
+          theme={barTheme}
+          data={data}
+          features={geoData.features}
+          projectionScale={isDashboard ? 100 : 150}
+          margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+          colors="nivo"
+          domain={[0, 1000000]}
+          unknownColor="#666666"
+          label="properties.name"
+          valueFormat=".2s"
+          projectionTranslation={[0.5, 0.5]}
+          projectionRotation={[0, 0, 0]}
+          enableGraticule={false}
+          graticuleLineColor="#dddddd"
+          borderWidth={0.5}
+          borderColor="#152538"
+          defs={[
+            {
+              id: "dots",
+              type: "patternDots",
+              background: "inherit",
+              color: "#38bcb2",
+              size: 4,
+              padding: 1,
+              stagger: true,
             },
-            id: "dots",
-          },
-          {
-            match: {
-              id: "CHN",
+            {
+              id: "lines",
+              type: "patternLines",
+              background: "inherit",
+              color: "#eed312",
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10,
             },
-            id: "lines",
-          },
-          {
-            match: {
-              id: "ATA",
-            },
-            id: "gradient",
-          },
-        ]}
-        legends={
-          isDashboard
-            ? []
-            : [
+            {
+              id: "gradient",
+              type: "linearGradient",
+              colors: [
                 {
-                  anchor: "bottom-left",
-                  direction: "column",
-                  justify: true,
-                  translateX: 20,
-                  translateY: -100,
-                  itemsSpacing: 0,
-                  itemWidth: 94,
-                  itemHeight: 18,
-                  itemDirection: "left-to-right",
-                  itemTextColor: "#444444",
-                  itemOpacity: 0.85,
-                  symbolSize: 18,
-                  effects: [
-                    {
-                      on: "hover",
-                      style: {
-                        itemTextColor: "#000000",
-                        itemOpacity: 1,
-                      },
-                    },
-                  ],
+                  offset: 0,
+                  color: "#000",
                 },
-              ]
-        }
-      />
-    </Stack>
+                {
+                  offset: 100,
+                  color: "inherit",
+                },
+              ],
+            },
+          ]}
+          fill={[
+            {
+              match: {
+                id: "CAN",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "CHN",
+              },
+              id: "lines",
+            },
+            {
+              match: {
+                id: "ATA",
+              },
+              id: "gradient",
+            },
+          ]}
+          legends={
+            isDashboard
+              ? []
+              : [
+                  {
+                    anchor: "bottom-left",
+                    direction: "column",
+                    justify: true,
+                    translateX: 20,
+                    translateY: -100,
+                    itemsSpacing: 0,
+                    itemWidth: 94,
+                    itemHeight: 18,
+                    itemDirection: "left-to-right",
+                    itemTextColor: "#444444",
+                    itemOpacity: 0.85,
+                    symbolSize: 18,
+                    effects: [
+                      {
+                        on: "hover",
+                        style: {
+                          itemTextColor: "#000000",
+                          itemOpacity: 1,
+                        },
+                      },
+                    ],
+                  },
+                ]
+          }
+        />
+      </Stack>
+    </Box>
   );
 }
